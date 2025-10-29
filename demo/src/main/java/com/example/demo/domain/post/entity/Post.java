5da1f7f7dce913df0650b67a32b82ac8cca0a944
cw01483-ly <cw01483@gmail.com>
2025-10-29 21:12:31 +0900

    [feat]Post.java 구성 및 기본썸네일 추가

--- demo/src/main/java/com/example/demo/domain/post/entity/Post.java
+++ demo/src/main/java/com/example/demo/domain/post/entity/Post.java
@@ -1,8 +1,10 @@
 package com.example.demo.domain.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
+import com.example.demo.domain.user.entity.User;
+import com.example.demo.global.base.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -14,6 +16,7 @@ import org.hibernate.annotations.Where;
 @AllArgsConstructor //모든 필드를 인자로 받는 생성자 자동 생성
 @Builder
 @ToString(exclude = "author") //author필드를 출력에서 제외(순환 참조 방지)
+// author는 @ManyToOne관계(지연로딩) > 예기치 않은 DB호출로 성능저하, 재귀순환문제 생기는것을 예방
 @EqualsAndHashCode(of = "id") // id 필드만으로 동일성 비교 (엔티티 루프 방지)
 @Table(
         name = "posts", // 실제 테이블명 지정 (post는 예약어이므로 복수형 사용)
@@ -22,10 +25,106 @@ import org.hibernate.annotations.Where;
                 @Index(name = "idx_posts_title", columnList = "title") // 제목 기준 검색 인덱스
         }
 )
-@SQLDelete(sql = "UPDATE posts SET is_deleted = true WHERE id = ?")
+@SQLDelete(sql = "UPDATE posts SET is_deleted = true WHERE id = ?") //삭제시 DELETE명령을 UPDATE로 바꿔 실행하기
 // delete 실행 시 실제 삭제 대신 is_deleted=true로 업데이트
-@Where(clause = "is_deleted = false")
-// 항상 is_deleted=false인 데이터만 조회되도록 필터링
+/* 사용자에겐 보이지 않고 운영자에겐 보이게 함
+    DB에서 삭제되면 복원 불가능, 이로 인해 통계,로그감시, 혹은 참조 무결성 문제에 영향을 끼치게 됨
+    >> 삭제 명령이 들어오면 UPDATE 문으로 바꿔서 실행시킴*/
+@Where(clause = "is_deleted = false")// 조회할 때 항상 WHERE is_deleted=false 조건을 자동으로 붙이기
+/* 항상 is_deleted=false인 데이터만 조회되도록 필터링
+    >> 조회할 때 삭제되지 않은(is_deleted = false)데이터만 보여줘라!
+    즉 'SQLDelete'와'Where' 으로 SoftDelete 시스템 구현*/
+/*
+SoftDelete 의 장점 ?
+    1) 실수로 삭제한다 해도 is_delete=false로 되돌리면 복원 가능
+    2) 언제, 누가 삭제했는지 로그 남길 수 있음
+    3) 관계 무결성 유지
+    4) 통계 유지
+SoftDelete 의 단점 ?
+    1) 데이터의 누적 (오래된 데이터는 백업 후 물리 삭제 배치 처리할 것)
+    2) 중복 문제(Unique제약 >> UNIQUE 조건에 is_deleted 포함 시키기)
+    3) 복잡한 쿼리 필요함 (Where 로 자동 필터링 혹은 JPQL에서 명시적 조건 사용하기)
+    */
+public class Post extends BaseTimeEntity { //BaseTimeEntity를 상속하여 시간설정 자동관리
+    // Post의 DB테이블에 자동으로 createAt, updatedAt 자동 생성
 
-public class Post {
+    @Id //Pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // INSERT시점에 PK 자동 증가,생성
+    private Long id;
+
+    @Column(nullable = false)
+    private Long displayNumber; //게시글 번호 , 게시글 삭제 후 재정렬을 위해 id와 별도 생성
+
+    //게시글 제목
+    @NotBlank(message = "제목은 공백일 수 없습니다.")
+    @Size(max=100,message="제목은 100자 이내로 입력해주세요")
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    //게시글 내용
+    @NotBlank(message = "내용을 입력하세요.")
+    @Column(nullable = false,columnDefinition = "TEXT") // DB컬럼 타입을 TEXT로 명시하여 대용량 텍스트를 지원.
+    private String content;
+
+    //작성자 정보
+    @ManyToOne(fetch = FetchType.LAZY, optional = false) //User(작성자)와 N:1 설정(지연로딩설정,부모없으면 금지)
+    @JoinColumn(name="user_id",nullable = false,foreignKey = @ForeignKey(name="fk_posts_user"))
+    //외래키(FK)설정, null 허용X
+    /*
+        JoinColumn(name="user_id" : posts.user_id라는 외래키 컬럼을 만들어 users.id참조
+        foreignKey = @ForeignKey(name="fk_posts_user" : FK 제약조건의 이름 명시
+        >> Post테이블에 user_id라는 FK 컬럼을 만들고, 반드시 users.id를 참조하게 하라 == 작성자 필수)
+     */
+    private User author; //작성자 정보는 User엔티티를 참조
+
+    //조회수 카운트
+    @Column(nullable = false)
+    @Builder.Default //Lombok의 @Builder는 필드 초기값을 무시함(값을 안넣으면 디폴트 미초기화상태 혹은 null)
+    private int views=0; //조회수의 처음값 0
+
+    //논리 삭제 여부(false - 정상, true = 삭제)
+    @Column(name = "is_deleted", nullable = false)
+    @Builder.Default
+    private boolean isDeleted=false;
+
+    /*
+                        비지니스 메서드 영역
+     */
+
+    /*
+        @PrePersist : INSERT 직전 실행
+        @PreUpdate : UPDATE 직전 실행 (수정 시간, 로그 갱신 등)
+        @PreRemove : DELETE 직전 실행 (삭제 로그 남기기)
+     */
+
+    @PrePersist
+    /*엔티티가 처음 저장(Persist)되기 전 자동으로 실행됨
+        > 게시글이 처음 저장될 때 displayNumber가 지정되지 않았다면 기본값을 부여
+         > 나중에 서비스 계층에서 직접 계산해서 setDisplayNumber() 호출할 수 있음*/
+    public void assignDefaultDisplayNumber(){
+        if(this.displayNumber==null){
+            this.displayNumber = 0L; //기본값 0으로 설정(임시값)
+        }
+    }
+
+    //게시글 수정 메서드
+    public void update(String title, String content){
+        if(title != null && !title.isBlank()) this.title = title.trim();
+        if(content != null && !content.isBlank()) this.content = content.trim();
+        //제목 혹은 컨텐츠가 공백이 아닐때 변경되도록 조건부 설치, 문자열의 앞뒤 공백은 제거(trim)
+    }
+
+    // 조회수 증가 메서드
+    public void increaseViews(){
+        //게시글 열람시 호출되어 views값 1 증가
+        this.views++;
+    }
+
+    //삭제 수행 메서드 (실제 DB삭제 X )
+    public void delete(){
+        this.isDeleted=true;
+        //DB에서 실제로 삭제하지 않고 isDeleted 값을 true로 변경하여 논리삭제 처리
+    }
+    
+
 }
