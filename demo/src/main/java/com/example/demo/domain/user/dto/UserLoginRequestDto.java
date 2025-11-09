d10c98a2eec2c6770df9ff8c119f507b523db689
cw01483-ly <cw01483@gmail.com>
2025-11-09 23:39:20 +0900

    [feat] UserLoginRequestDto 생성 및 로그인 요청 검증 추가

---
+++ demo/src/main/java/com/example/demo/domain/user/dto/UserLoginRequestDto.java
@@ -0,0 +1,23 @@
+package com.example.demo.domain.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter // username,password에 대한 getter 자동생성
+@NoArgsConstructor // JSON > 객체변환
+@AllArgsConstructor // 모든 필드를 받는 자동 생성
+//로그인 요청 데이터를 담는 Dto
+public class UserLoginRequestDto {
+
+    @NotBlank(message = "ID는 필수 입력 값입니다.")
+    @Size(min = 4, max= 20,message = "ID는 3자 이상 20자 이하 입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Size(min = 8, max = 50, message = "비밀번호는 8자 이상 50자 이하로 입력해주세요.")
+    private String password;
+
+}
