743ef8bc025d1d08164920e58e9742ccc78ec0a4
cw01483-ly <cw01483@gmail.com>
2025-12-01 19:35:21 +0900

    [refactor] ApiResponse 구조 개선 및 실패 응답 데이터 처리 수정

--- demo/src/main/java/com/example/demo/global/response/ApiResponse.java
+++ demo/src/main/java/com/example/demo/global/response/ApiResponse.java
@@ -27,7 +27,6 @@ public class ApiResponse<T> {
     private final T data;
 
     // 요청 성공시 사용하는 정적 메서드 ex) data : UserResponseDto
-
     public static <T> ApiResponse<T> success(T data, String message){
         //       선언부     리턴타입           매개변수타입
         return ApiResponse.<T>builder() // ApiResponse 객체를 빌더 패턴으로 생성
@@ -47,13 +46,22 @@ public class ApiResponse<T> {
     }
 
     // 요청이 실패했을 때 사용하는 정적 메서드 ex) GlobalExceptionHandler 에서 사용
-    // 실패 원인 메시지를 반환
-
-    public static <T> ApiResponse<T> fail(String message){
+    // 실패 원인 메시지를 반환, data 자리에 ErrorResponse 객체가 들어감
+    public static <T> ApiResponse<T> fail(T data,String message){
         return ApiResponse.<T>builder()
                 .success(false)     // 요청 실패처리
                 .message(message)   // 실패 메시지 설정
-                .data(null)         // 실패 응답엔 데이터 x
+                .data(data)         // 에러 상세정보 그대로 담아줌
                 .build();
     }
+
+    // 메시지만 있고 data가 필요 없을 때 사용할 실패응답 (ex : ApiResponse.fail("권한이 없습니다")
+    public static <T> ApiResponse<T> fail(String message) {
+        return ApiResponse.<T>builder()
+                .success(false)
+                .message(message)
+                .data(null)
+                .build();
+    }
+
 }
