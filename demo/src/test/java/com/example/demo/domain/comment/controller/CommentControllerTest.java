7eb95e2165bb90883e8c8a5859ca66c095ed4782
cw01483-ly <cw01483@gmail.com>
2025-12-10 21:34:01 +0900

    test: CommentControllerTest commentDelete 실패 테스트(비로그인) 추가

--- demo/src/test/java/com/example/demo/domain/comment/controller/CommentControllerTest.java
+++ demo/src/test/java/com/example/demo/domain/comment/controller/CommentControllerTest.java
@@ -635,6 +635,47 @@ public class CommentControllerTest {
 
 
 
+    // ⭐ 댓글 삭제 실패 테스트
+    @Test
+    @DisplayName("댓글 삭제 실패 : 비로그인 사용자는 401 Unauthorized가 발생")
+    void deleteComment_fail_unauthenticated() throws Exception{
+        // [GIVEN] 유저, 게시글, 댓글 생성
+        User user = userRepository.save(
+                User.builder()
+                        .username("username1")
+                        .password("Password123!")
+                        .nickname("nickname")
+                        .email("email@example.com")
+                        .build()
+        );
+        Post post = postRepository.save(
+                Post.builder()
+                        .title("title")
+                        .content("content")
+                        .author(user)
+                        .displayNumber(1L)
+                        .build()
+        );
+        Comment comment = commentRepository.save(
+                Comment.builder()
+                        .post(post)
+                        .author(user)
+                        .content("삭제 전")
+                        .build()
+        );
+        Long commentId = comment.getId(); // 삭제 시도할 댓글 ID
+
+        // [WHEN & THEN] 로그인 정보 없이 DELETE 요청 → 401 기대
+        mockMvc.perform(
+                delete("/api/comments/{commentId}", commentId)
+                // .with() 없이 >> 비로그인 상태
+        )
+                .andDo(print())
+                .andExpect(status().isUnauthorized()); // HTTP 401 Unauthorized
+    }
+
+
+
 
 
 
