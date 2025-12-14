b4857938dc5e30c83b5e25b5ab840ce20b4e8a7c
cw01483-ly <cw01483@gmail.com>
2025-12-15 01:28:59 +0900

    feat(security): 비로그인 401 처리를 위한 JwtAuthenticationEntryPoint 추가

---
+++ demo/src/main/java/com/example/demo/global/security/jwt/handler/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package com.example.demo.global.security.jwt.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/*
+    JwtAuthenticationEntryPoint
+    - "비로그인(인증 실패)" 상황에서 Spring Security가 호출하는 진입점(EntryPoint)
+    -  비로그인 = 401
+    - API 서버 관점에서 redirect(302) 같은 동작 대신, 명확히 401로 내려주기 위해 사용
+ */
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException, ServletException{
+
+        // 어떤 요청이 인증 실패로 떨어졌는지 로그로 남기기 (비로그인)
+        log.debug("[SECURITY] 401 Unauthorized. uri={}, message={}",
+                request.getRequestURI(),
+                authException.getMessage()
+        );
+
+        // 비로그인(인증X) 이므로 401로 통일
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
