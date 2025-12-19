1c3e87003cc256d7517829bcec61c0e7991a7ea9
cw01483-ly <cw01483@gmail.com>
2025-12-19 21:43:45 +0900

    feat(home): 홈에서 JWT 로그인 상태 표시 및 로그아웃 버튼 추가

--- demo/src/main/java/com/example/demo/domain/ui/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/domain/ui/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.example.demo.domain.ui.controller;
 
+
+import com.example.demo.global.security.CustomUserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(){
+    public String home(
+            @AuthenticationPrincipal CustomUserDetails principal,
+            Model model
+    ){
+        // 로그인 여부 전달
+        model.addAttribute("isLogin",principal != null);
+
+        // 로그인 상태인 경우 username 전달
+        model.addAttribute("username",principal != null ? principal.getUsername() : null);
+
         return "demo/index";
     }
 }
